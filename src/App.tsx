c30c7710bae815b5cf2b6401a990daa3c20cb79b
Lyran Vynlz <lyranvynlz@gmail.com>
2025-07-01 23:42:45 +0630

    First Comment

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { Link, NavLink, Outlet } from "react-router"
+
+export default function App() {
+   return (
+     <>
+       <NavBar/>
+
+       <main className="container mt-4">
+          <Outlet />
+       </main>
+     </>
+   )
+}
+
+function NavBar() {
+   return (
+      <nav className="navbar navbar-dark bg-black navbar-expand">
+          <div className="container">
+             <Link className="navbar-brand" to ="/">
+                  <i className="bi-house"></i>
+                  Task Manager
+              </Link>
+           
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink to="/" className="nav-link">
+                        <i className="bi-rocket-takeoff"></i>Projects
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/" className="nav-link">
+                        <i className="bi-stack"></i>Stack
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/" className="nav-link">
+                        <i className="bi-people"></i>Members
+                    </NavLink>
+                </li>
+              </ul>
+           </div>
+      </nav>
+      )
+}
